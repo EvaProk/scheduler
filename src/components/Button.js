1643208b8ef1c23ae36b4ca526691693eddc4e7f
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-28 15:11:41 -0700

    Add button states

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,28 @@
 import React from "react";
+import classnames from 'classnames';
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = classnames({
+      "button" :true,
+      " button--confirm": props.confirm,
+      " button--danger": props.danger
+
+   });
+ 
+   return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
 }
+
+
+// let buttonClass = "button";
+
+//    if (props.confirm) {
+//      buttonClass += " button--confirm";
+//    }else if(props.danger) {
+//       buttonClass += " button--danger";
+//    }
+ 
+//    return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
+// }
+
